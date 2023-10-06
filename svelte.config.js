3d971f6cdc50256db4346564a72bdd274cfa68d8
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-10-06 13:13:28 +0200

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'docs'
+		})
+	}
+};
+
+export default config;
